4fc9e0382dc540defbfeeb5d5d78d01cab04717e
Rob Nickmans <rob.nickmans@rndevelopment.be>
2022-03-03 20:22:33 +0100

    established connection

--- flutter/bean_bot/lib/main.dart
+++ flutter/bean_bot/lib/main.dart
@@ -1,24 +1,64 @@
 import 'package:bean_bot/data/menu_items.dart';
 import 'package:bean_bot/model/menu_item.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import 'debug.dart';
 import 'logs.dart';
+import 'mqtt/MQTTManager.dart';
+import 'mqtt/state/MQTTAppState.dart';
 
 void main() {
-  runApp(const MaterialApp(
+  runApp(MaterialApp(
     title: 'The Bean Bot',
-    home: BeanBot(),
+    home: ChangeNotifierProvider<MQTTAppState>(
+      create: (_) => MQTTAppState(),
+      child: const BeanBot(),
+    ),
   ));
 }
 
-class BeanBot extends StatelessWidget {
+class BeanBot extends StatefulWidget {
   const BeanBot({Key? key}) : super(key: key);
 
+  @override
+  _BeanBotState createState() => _BeanBotState();
+}
+
+class _BeanBotState extends State<BeanBot> {
   static const String _title = 'The Bean Bot';
+
+  late MQTTAppState currentAppState;
+  late MQTTManager manager;
+
+  void _configureAndConnect() {
+    currentAppState.setHostIp('192.168.0.193');
+    manager = MQTTManager(
+        host: currentAppState.getHostIP,
+        topic: "order",
+        identifier: "BeanBotDemo",
+        state: currentAppState);
+    manager.initializeMQTTClient();
+    manager.connect();
+  }
+
+  String _prepareStateMessageFrom(MQTTAppConnectionState state) {
+    switch (state) {
+      case MQTTAppConnectionState.connected:
+        return 'Connected';
+      case MQTTAppConnectionState.connecting:
+        return 'Connecting';
+      case MQTTAppConnectionState.disconnected:
+        return 'Disconnected';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    final MQTTAppState appState = Provider.of<MQTTAppState>(context);
+    // Keep a reference to the app state.
+    currentAppState = appState;
+
     return MaterialApp(
       title: _title,
       home: Scaffold(
@@ -32,8 +72,11 @@ class BeanBot extends StatelessWidget {
               ),
             ],
           ),
-          body: ListView(children: const [
-            WeightInput(),
+          body: ListView(children: [
+            _buildConnectionStateText(_prepareStateMessageFrom(
+                currentAppState.getAppConnectionState)),
+            _buildWeightInput(),
+            _buildConfirmButtons(currentAppState.getAppConnectionState),
           ])),
     );
   }
@@ -51,25 +94,30 @@ class BeanBot extends StatelessWidget {
         break;
       case MenuItems.itemLog:
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => LogPage()),
+          MaterialPageRoute(builder: (context) => const LogPage()),
         );
         break;
     }
   }
-}
 
-class WeightInput extends StatefulWidget {
-  const WeightInput({Key? key}) : super(key: key);
-
-  @override
-  _WeightInputState createState() => _WeightInputState();
-}
-
-class _WeightInputState extends State<WeightInput> {
   final _weightForm = GlobalKey<FormState>();
 
-  @override
-  Widget build(BuildContext context) {
+  Widget _buildConnectionStateText(String status) {
+    return Row(
+      children: <Widget>[
+        Expanded(
+          child: Container(
+              color: Colors.deepOrangeAccent,
+              child: Padding(
+                padding: const EdgeInsets.all(4.0),
+                child: Text(status, textAlign: TextAlign.center),
+              )),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildWeightInput() {
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _weightForm,
@@ -96,46 +144,52 @@ class _WeightInputState extends State<WeightInput> {
             padding: EdgeInsets.symmetric(horizontal: 8, vertical: 16),
             child: BeanKindDropdown(),
           ),
-          Row(
-            children: [
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Dismisses keyboard
-                      FocusScopeNode currentFocus = FocusScope.of(context);
-                      if (!currentFocus.hasPrimaryFocus) {
-                        currentFocus.unfocus();
-                      }
-                      // Validate returns true if the form is valid, or false otherwise.
-                      if (_weightForm.currentState!.validate()) {
-                        // If the form is valid, display a snackbar. In the real world,
-                        // you'd often call a server or save the information in a database.
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Processing Data')),
-                        );
-                      }
-                    },
-                    child: const Text('SUBMIT'),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ElevatedButton(
-                    onPressed: () {
-                      // Respond to button press
-                    },
-                    child: const Text('CANCEL'),
-                  ),
-                ),
-              ),
-            ],
-          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildConfirmButtons(MQTTAppConnectionState state) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                if (state == MQTTAppConnectionState.disconnected) {
+                  _configureAndConnect();
+                }
+                // Dismisses keyboard
+                FocusScopeNode currentFocus = FocusScope.of(context);
+                if (!currentFocus.hasPrimaryFocus) {
+                  currentFocus.unfocus();
+                }
+                // Validate returns true if the form is valid, or false otherwise.
+                if (_weightForm.currentState!.validate()) {
+                  // If the form is valid, display a snackbar. In the real world,
+                  // you'd often call a server or save the information in a database.
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    const SnackBar(content: Text('Processing Data')),
+                  );
+                }
+              },
+              child: const Text('SUBMIT'),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () {
+                // Respond to button press
+              },
+              child: const Text('CANCEL'),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }

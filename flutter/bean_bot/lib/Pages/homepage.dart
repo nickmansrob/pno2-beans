1f1d426ac27696da3456058474d0298a13c1ebe4
Rob Nickmans <rob.nickmans@rndevelopment.be>
2022-03-13 14:24:54 +0100

    Changed icon (#23)

--- flutter/bean_bot/lib/Pages/homepage.dart
+++ flutter/bean_bot/lib/Pages/homepage.dart
@@ -597,7 +597,7 @@ class _HomePageState extends State<HomePage> {
         topic1: "order",
         topic2: "logListener",
         topic3: "weightListener",
-        identifier: "BeanBotDemo",
+        identifier: "FlutterBeanBot",
         state: currentAppState,
         orderState: currentOrderState);
     manager.initializeMQTTClient();

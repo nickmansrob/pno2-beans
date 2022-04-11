2e1f679b74881614c41a7f261e52d8e5338c8e1e
Robin Martens <robin.martens@quivr.be>
2022-04-11 21:11:18 +0200

    Some progress on color calibration in app

--- flutter/bean_bot/lib/data/menu_items.dart
+++ flutter/bean_bot/lib/data/menu_items.dart
@@ -4,11 +4,15 @@ class MenuItems {
   static const List<MenuItem> items = [
     itemDebug,
     itemLog,
+    itemColor
   ];
   static const itemDebug = MenuItem(
     text: 'Debug Menu',
   );
   static const itemLog = MenuItem(
     text: 'Logs',
+  );
+  static const itemColor = MenuItem(
+    text: 'Color Calibration',
   );
 }

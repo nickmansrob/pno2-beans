239cefbdb166638e27cb283048bb11e1543cbd82
Robin Martens <robin.martens@quivr.be>
2022-04-14 14:17:01 +0200

    .

--- flutter/bean_bot/lib/Providers/color_calibration_state.dart
+++ flutter/bean_bot/lib/Providers/color_calibration_state.dart
@@ -5,6 +5,9 @@ class ColorCalibrationState with ChangeNotifier {
   int _g = 0;
   int _b = 0;
   int _calibrationsDone = 0;
+  bool _startCalibration = false;
+  String _calibrationSentMessage = 'start000000000';
+  String _calibrationReceivedMessage = 'stop256256256';
 
   void set_r(int r) {
     _r = r;
@@ -26,8 +29,26 @@ class ColorCalibrationState with ChangeNotifier {
     notifyListeners();
   }
 
+  void setStartCalibration(value) {
+    _startCalibration = value;
+    notifyListeners();
+  }
+
+  void setCalibrationSentMessage(message) {
+    _calibrationSentMessage = message;
+    notifyListeners();
+  }
+
+  void setCalibrationReceivedMessage(message) {
+    _calibrationReceivedMessage = message;
+    notifyListeners();
+  }
+
   int get get_r => _r;
   int get get_g => _g;
   int get get_b => _b;
   int get getCalibrationsDone => _calibrationsDone;
+  bool get getStartCalibration => _startCalibration;
+  String get getCalibrationSentMessage => _calibrationSentMessage;
+  String get getCalibrationReceivedMessage => _calibrationReceivedMessage;
 }
